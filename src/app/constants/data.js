3717130d71087917f6233315397c5b1765342023
officialyaro9 <ridwanyinusa15@gmail.com>
2023-12-19 01:24:25 +0100

    Initial commit from Create Next App

---
+++ src/app/constants/data.js
@@ -0,0 +1,59 @@
+import amarone from '@assets/amarone.png'
+
+export const Products = [
+  {
+    imgUrl: amarone,
+    name: "Dolceto",
+    price: "12,400",
+  },
+  {
+    imgUrl: "",
+    name: "Merlot Clearsprings",
+    price: "15,900",
+  },
+  {
+    imgUrl: "",
+    name: "Yering",
+    price: "11,800",
+  },
+  {
+    imgUrl: "",
+    name: "Pinot Noir: Aymura",
+    price: "12,900",
+  },
+  {
+    imgUrl: "",
+    name: "Sauvignon Blanc",
+    price: "10,600",
+  },
+  {
+    imgUrl: "",
+    name: "Veuve Cliquot",
+    price: "11,000",
+  },
+  {
+    imgUrl: "",
+    name: "Amarone",
+    price: "12,400",
+  },
+  {
+    imgUrl: "",
+    name: "Vina Ardanza",
+    price: "12,500",
+  },
+  {
+    imgUrl: "",
+    name: "Nebla",
+    price: "14,400",
+  },
+  {
+    imgUrl: "",
+    name: "Canaletto",
+    price: "13,200",
+  },
+  {
+    imgUrl: "",
+    name: "Clearsprings",
+    price: "10,100",
+  },
+];
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+import amarone from "@assets/amarone.png";
+
+export default function Home() {
+  return <main><img src={amarone} alt="wine" /></main>;
+}
