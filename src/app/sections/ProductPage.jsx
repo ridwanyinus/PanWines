249e0c59555efbaad9d5b264c4ae1fd3e472483a
officialyaro9 <ridwanyinusa15@gmail.com>
2023-12-21 17:29:03 +0100

    Images_Width2

--- src/app/sections/ProductPage.jsx
+++ src/app/sections/ProductPage.jsx
@@ -13,9 +13,9 @@ const ProductPage = () => {
             <Image
               src={item.imgUrl}
               alt={item.name}
-              className="w-auto h-auto  md:w-[40vw] lg:w-[25vw] xl:w-[370px] widescreen:w-[400px] sm:w-[38vw] small:w-[70vw] small:h-[55vh] max-small:h-[30vh] max-small:w-[80vw]  cursor-pointer relative"
+              className="w-auto h-auto  md:w-[40vw] lg:w-[25vw] xl:w-[370px] widescreen:w-[400px] sm:w-[38vw] small:w-[70vw] xl:h-[60vh] small:h-[40vh] max-small:h-[30vh] max-small:w-[85vw]  cursor-pointer relative"
             />
-            <p className="text-primary leading-[116.667%]  text-base lg:text-xl xl:text-2xl font-medium mt-4">{item.name}</p>
+            <p className="text-primary leading-[116.667%]  text-base lg:text-xl xl:text-2xl font-medium mt-3">{item.name}</p>
             <p className="mt-1 sm:mt-2 xl:mt-3 text-primary leading-[116.667%] text-xs lg:text-base xl:text-[20px] font-medium ">${item.price}</p>
             <Link href="/products/shoppingCart/cart">
               <button
