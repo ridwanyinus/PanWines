ef7802be78d0a4e2abb61a68db51537ada11fbef
officialyaro9 <ridwanyinusa15@gmail.com>
2023-12-22 14:06:33 +0100

    Navmenu

--- src/app/products/shoppingCart/page.jsx
+++ src/app/products/shoppingCart/page.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 const page = () => {
   return (
-    <section className="flex flex-col justify-center items-center h-screen w-full">
+    <section className="flex h-screen flex-col justify-center items-center  w-full">
       <h1 className="text-primary font-bold text-2xl xs:text-3xl sm:text-4xl md:text-5xl leading-[125%] text-center mb-2 max-lg:mb-4">Your Shopping Cart is Empty</h1>
       <p className="text-primary font-medium text-[13px] xs:text-base sm:text-lg md:text-[20px] leading-[150%]">Explore our product page to add item(s) to your cart</p>
       <Link href="/products">
--- src/app/sections/Navbar.jsx
+++ src/app/sections/Navbar.jsx
@@ -1,29 +1,27 @@
 "use client";
+import React from "react";
 import Link from "next/link";
-
 import { AiOutlineShoppingCart } from "react-icons/ai";
-
 import { FiAlignRight } from "react-icons/fi";
 import { IoClose } from "react-icons/io5";
-
 import { useState } from "react";
 
 const Navbar = () => {
   const [toggleMenu, settoggleMenu] = useState(false);
 
   return (
-    <nav className="bg-primary px-6 py-4 lg:px-12 lg:py-6 flex items-center  justify-between ">
-      <Link href="/" className="text-white font-bold  text-2xl lg:text-3xl  leading-[40px] ">
+    <nav className="bg-primary px-6 py-5 lg:px-12 lg:py-6 flex lg:items-center  justify-between ">
+      <Link href="/" className="text-white font-bold  text-2xl lg:text-3xl  lg:leading-[40px] ">
         PanWines
       </Link>
-      <div className="flex gap-6 xl:gap-8 max-lg:hidden">
+      <div className="flex gap-6 xl:gap-8 max-lg:hidden w-fit h-full">
         <Link href="/" className="text-xl text-white opacity-95  font-medium leading-[116.667%] hover:border-b border-white hover:opacity-100">
           Home
         </Link>
         <Link href="/products" className="text-xl text-white opacity-95  font-medium leading-[116.667%] hover:border-b border-white hover:opacity-100">
           Product
         </Link>
-        <Link href="/products/shoppingCart" className="text-xl text-white opacity-95  font-medium leading-[116.667%]  hover:opacity-100 flex">
+        <Link href="/products/shoppingCart" className="text-xl text-white opacity-95  font-medium leading-[116.667%]  hover:bg-slate-50 hover:px-2 hover:rounded hover:bg-opacity-5 w-full flex">
           Shopping Cart
           <span className="pl-[2px]">
             <AiOutlineShoppingCart width={32} height={28} />
@@ -45,34 +43,54 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="flex  lg:hidden  relative justify-between items-center h-[100%]">
-        {toggleMenu ? (
-          <IoClose size={27} color="#fff" onClick={() => settoggleMenu(false)} />
-        ) : (
-          <FiAlignRight
-            size={27}
-            color="#fff"
-            onClick={() => {
-              console.log("Hello");
-            }}
-            // onClick={() => settoggleMenu(true)}
-          />
-        )}
+      <div className="flex flex-col lg:hidden gap-6 h-[100%]  ">
+        <div className=" w-full h-full flex items-end justify-end ">
+          {toggleMenu ? (
+            <div className="border border-slate-50 border-opacity-50 rounded px-1  ">
+              {" "}
+              <IoClose size={28} color="#fff" onClick={() => settoggleMenu(false)} />
+            </div>
+          ) : (
+            <div className="border border-slate-50 border-opacity-50 rounded p-1 px-[6px] ">
+              <FiAlignRight size={27} color="#fff" onClick={() => settoggleMenu(true)} />
+            </div>
+          )}
+        </div>
 
         {toggleMenu && (
-          <div className="flex gap-6 xl:gap-8 lg:hidden">
-            <Link href="/" className="text-xl text-primary opacity-95  font-medium leading-[116.667%] hover:border-b border-white hover:opacity-100">
+          <div className="flex flex-col justify-end bg-primary rounded  w-full gap-2 items-end scale-up-ver-top">
+            <Link href="/products" className="text-base small:text-lg  text-white opacity-95  font-normal leading-[116.667%] hover:border-b border-white hover:opacity-100">
               Home
             </Link>
-            <Link href="/products" className="text-xl text-white opacity-95  font-medium leading-[116.667%] hover:border-b border-white hover:opacity-100">
+            <Link href="/products" className="text-base small:text-lg  text-white opacity-95  font-normal leading-[116.667%] hover:border-b border-white hover:opacity-100">
               Product
             </Link>
-            <Link href="/shoppingCart" className="text-xl text-white opacity-95  font-medium leading-[116.667%]  hover:opacity-100 flex">
+            <Link
+              href="/shoppingCart"
+              className="text-base small:text-lg  text-white opacity-95  font-normal leading-[116.667%] hover:border-b border-white hover:opacity-100 flex justify-center items-center">
               Shopping Cart
               <span className="pl-[2px]">
                 <AiOutlineShoppingCart width={32} height={28} />
               </span>
             </Link>
+
+            <div className="flex flex-col max-sm:mt-1 gap-3 justify-end items-end">
+              <Link href="/login">
+                <button
+                  type="button"
+                  className="text-primary bg-white text-base small:text-lg font-medium leading-[116.667%] rounded small:rounded-lg py-1 px-5 active:bg-transparent active:border active:text-white">
+                  Login
+                </button>
+              </Link>
+
+              <Link href="/signUp">
+                <button
+                  type="button"
+                  className="text-primary bg-white text-base small:text-lg font-medium leading-[116.667%] rounded small:rounded-lg py-1 px-5 active:bg-transparent active:border active:text-white">
+                  Sign Up
+                </button>
+              </Link>
+            </div>
           </div>
         )}
       </div>
